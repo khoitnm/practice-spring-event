5ef503972be6ceef63cfb5db574e7d9224229b3b
Kevin Tran <kevin.tran@leonardo.com>
2018-06-15 13:22:51 -0400

    Initiate project

---
+++ pro01-application-event/src/main/java/org/tnmk/practice/springgrpc/pro01applicationevent/service/Item.java
@@ -0,0 +1,22 @@
+package org.tnmk.practice.springgrpc.pro01applicationevent.service;
+
+public class Item {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
